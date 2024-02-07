bedc033cea8e6de6e17e124df895771c603cbb4f
Nathan Damm <ndamm2006@gmail.com>
2024-02-07 11:14:41 -0500

    Added Proper CAN IDs

--- YAGSL Test Build/src/main/java/frc/robot/Constants.java
+++ YAGSL Test Build/src/main/java/frc/robot/Constants.java
@@ -20,7 +20,7 @@ import swervelib.parser.PIDFConfig;
 public final class Constants
 {
 
-  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+  public static final double ROBOT_MASS = (30) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
@@ -30,7 +30,7 @@ public final class Constants
     public static final PIDFConfig TranslationPID = new PIDFConfig(0.7, 0, 0);
     public static final PIDFConfig angleAutoPID   = new PIDFConfig(0.4, 0, 0.01);
 
-    public static final double MAX_ACCELERATION = 2;
+    public static final double MAX_ACCELERATION = 4;
   }
 
   public static final class Drivebase
