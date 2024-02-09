1387a169f0dd021b901cfef59ee23b22698d2722
Nathan Damm <ndamm2006@gmail.com>
2024-02-09 12:35:39 -0500

    Worked on Auto-Chooser Code
    Still needs testing

--- YAGSL Test Build/src/main/java/frc/robot/RobotContainer.java
+++ YAGSL Test Build/src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -19,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.swervedrive.drivebase.AbsoluteDriveAdv;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -52,8 +52,9 @@ public class RobotContainer
    */
   public RobotContainer()
   {
-    //Build Auto Chooser
+    //Build Auto-Chooser
     autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Chooser", autoChooser);
 
     // Configure the trigger bindings
     configureBindings();
@@ -126,10 +127,10 @@ public class RobotContainer
    *
    * @return the command to run in autonomous
    */
-  
-   //Gets auto from selection box 
-  public Command getAutonomousCommand() {
-    return autoChooser.getSelected();
+  public Command getAutonomousCommand()
+  {
+    // An example command will be run in autonomous
+    return drivebase.getAutonomousCommand(autoChooser.getSelected().toString(), true);
   }
 
   public void setDriveMode()
